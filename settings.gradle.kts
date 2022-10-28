49421af6a971def0b766601cfc988cc66b33713a
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2022-10-28 21:15:51 +0100

    defining a project structure

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+include("app")
+include("business-logic")
+include("data-model")
+
+pluginManagement {
+    repositories.gradlePluginPortal()
+//    repositories.mavenCentral()
+//    repositories.google()
+//    repositories.maven("https://my.location/repo") {
+//        credentials.username = "..."
+//        credentials.password = "..."
+//    }
+//
+//    includeBuild("gradle/plugins")
+}
+
+dependencyResolutionManagement {
+    repositories.mavenCentral()
+//    repositories.google()
+//    repositories.maven("https://my.location/repo") {
+//        credentials.username = "..."
+//        credentials.password = "..."
+//    }
+}
+
+rootProject.name = "my-project"
